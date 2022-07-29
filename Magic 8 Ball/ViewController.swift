d7b63d0e419a10e74120ae5255d7e96923102bc4
Matt Murphy <matt.murphy@blindrobot.com>
2022-07-29 11:02:41 -0400

    implement random 8 ball responses

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -1,18 +1,15 @@
-//
-//  ViewController.swift
-//  Magic 8 Ball
-//
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var magicView: UIImageView!
 
-
+    @IBAction func askMagicEightBall(_sender: UIButton) {
+        let ballArray = [ UIImage(named: "ball1.png"), UIImage(named:"ball2.png"), UIImage(named: "ball3.png"), UIImage(named: "ball4.png"), UIImage(named: "ball5.png")]
+        
+        magicView.image = ballArray[Int .random(in: 0...4)]
+        
+    }
 
 }
 
